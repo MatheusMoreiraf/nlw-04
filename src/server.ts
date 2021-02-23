f9842840ee67463440ec6b5d6511e687a232c07c
Matheus Moreira Ferreira <matheusmoreira.g6@gmail.com>
2021-02-23 20:30:13 -0300

    feat(user): route, controller and model for user

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,11 @@
-import "reflect-metadata";
-import express from "express";
-import "./database"
+import 'reflect-metadata';
+import express from 'express';
+import './database';
+import { router } from './routes';
 
 const app = express();
 
-/**
- * => Principais Metodos <=
- * GET => Buscar
- * POST => Salvar
- * PUT => Alterar
- * DELETE => Deletar
- * PATCH => Alteração especifica
- * */
-
-app.get("/", (request, response) => {
-    return response.json({ message: "Hello Word - NLW04" })
-})
-
-// 1 Param => Rota(Recurso API)
-// 2 Param => request, response
-
-app.post("/", (request, response) => {
-    // Recebeu os dados para salvar
-    return response.json({ message: "Os dados foram salvos com sucesso!"})
-})
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log('Server is running!'))
