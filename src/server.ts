7b734552939977ee0246fd69abd528304189ddbb
Matheus Moreira Ferreira <matheus@tog.design>
2021-02-23 10:46:47 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+
+const app = express();
+
+/**
+ * => Principais Metodos <=
+ * GET => Buscar
+ * POST => Salvar
+ * PUT => Alterar
+ * DELETE => Deletar
+ * PATCH => Alteração especifica
+ * */
+
+app.get("/", (request, response) => {
+    return response.json({ message: "Hello Word - NLW04" })
+})
+
+// 1 Param => Rota(Recurso API)
+// 2 Param => request, response
+
+app.post("/", (request, response) => {
+    // Recebeu os dados para salvar
+    return response.json({ message: "Os dados foram salvos com sucesso!"})
+})
+
+app.listen(3333, () => console.log('Server is running!'))
