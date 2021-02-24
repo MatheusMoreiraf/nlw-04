36c0c16b356cf0b17a7c8d602b8b12b9b7a9e42e
Matheus Moreira Ferreira <matheusmoreira.g6@gmail.com>
2021-02-24 19:52:57 -0300

    feat(survey): route, controller, repository, model and migration for create survey

---
+++ src/database/migrations/1614205244994-CreateSurveys.ts
@@ -0,0 +1,36 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateSurveys1614205244994 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "surveys",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true
+                    },
+                    {
+                        name: "title",
+                        type: "varchar"
+                    },
+                    {
+                        name: "description",
+                        type: "varchar"
+                    },
+                    {
+                        name: "crated_at",
+                        type: "timestamp",
+                        default: "now()"
+                    }
+                ]
+            })
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("surveys")
+    }
+
+}
