0e3a9e90c41cfd15f5055915ecee393a54b2cc97
Matheus Moreira Ferreira <matheusmoreira.g6@gmail.com>
2021-02-27 16:50:44 -0300

    feat(send-mail): service creation for sending emails

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -3,6 +3,7 @@ import { getCustomRepository } from 'typeorm';
 import { UserRepository } from '../repositories/UserRepository';
 import { SurveysRepository } from '../repositories/SurveysRepository';
 import { SurveysUsersRepository } from '../repositories/SurveysUsersRepository';
+import SendMailService from '../services/SendMailService';
 
 class SendMailController {
     async execute(request: Request, response: Response) {
@@ -20,9 +21,9 @@ class SendMailController {
             });
         }
 
-        const surveyAlreadyExists = await surveysRepository.findOne({ id: survey_id });
+        const survey = await surveysRepository.findOne({ id: survey_id });
 
-        if (!surveyAlreadyExists) {
+        if (!survey) {
             return response.status(400).json({
                 error: "Survey does not exists!",
             });
@@ -30,11 +31,11 @@ class SendMailController {
 
         const surveyUser = surveysUsersRepository.create({
             user_id: userAlreadyExists.id,
-            survey_id: surveyAlreadyExists.id,
+            survey_id: survey.id,
         });
         await surveysUsersRepository.save(surveyUser);
 
-        // TODO: send email
+        await SendMailService.execute(email, survey.title, survey.description);
 
         return response.status(201).json(surveyUser);
     }
