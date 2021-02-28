e06fb0ddf559cd3ce3ff4ab9bb4fce4dbb36a942
Matheus Moreira Ferreira <matheusmoreira.g6@gmail.com>
2021-02-27 22:31:01 -0300

    feat(nps): controller and route to calculate the net promoter score

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { UserController } from './controllers/UserController';
 import { SurveysController } from './controllers/SurveysController';
 import { SendMailController } from './controllers/SendMailController';
 import { AnswerController } from './controllers/AnswerController';
+import { NpsController } from './controllers/NpsController';
 
 const router = Router();
 
@@ -13,12 +14,16 @@ const sendMailController = new SendMailController();
 
 const answerController = new AnswerController();
 
+const npsController = new NpsController();
+
 router.post("/users", userController.create);
 router.post("/surveys", surveysController.create);
 router.get("/surveys", surveysController.show);
 
-router.post( "/send-mail", sendMailController.execute);
+router.post("/send-mail", sendMailController.execute);
 
-router.get("/answers/:value", answerController.execute)
+router.get("/answers/:value", answerController.execute);
+
+router.get("/nps/:survey_id", npsController.execute);
 
 export { router };
